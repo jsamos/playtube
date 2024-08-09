37d7ddc2780c17f705f70ea39925e1fb45f0782d
Juni Samos <juni@Junis-MacBook-Pro.local>
2024-08-09 13:42:54 +0200

    Add Mix Length

--- playlist_loader/app/main.py
+++ playlist_loader/app/main.py
@@ -1,11 +1,18 @@
 import argparse
 from app import cue
+from app import audio
 
 def load_tracks(cue_path):
     with open(cue_path) as f:
         cue_string = f.read()
     tracks = cue.parse_cue_tracks(cue_string)
     return tracks
+
+def add_mix_length(playlist):
+    audio_length = audio.get_audio_length(playlist['file'])
+    length = f"{audio_length // 3600:02d}:{(audio_length % 3600) // 60:02d}:{audio_length % 60:02d}"
+    playlist['length'] = length
+
 
 """
 takes and array of dictionaries and calculates the length of each track in mm:ss format
@@ -15,11 +22,13 @@ in the case of the last track lenghth = None
 def add_track_lengths(tracks):
     for i in range(len(tracks) - 1):
         current_track = tracks[i]
-        next_track = tracks[i + 1]
         current_index = current_track['index']
-        next_index = next_track['index']
         current_seconds = int(current_index[:2]) * 3600 + int(current_index[3:5]) * 60 + int(current_index[6:8])
+
+        next_track = tracks[i + 1]
+        next_index = next_track['index']
         next_seconds = int(next_index[:2]) * 3600 + int(next_index[3:5]) * 60 + int(next_index[6:8])
+        
         length_seconds = next_seconds - current_seconds
         length = f"{length_seconds // 60:02d}:{length_seconds % 60:02d}"
         tracks[i]['length'] = length
--- playlist_loader/tests/test_main.py
+++ playlist_loader/tests/test_main.py
@@ -2,30 +2,50 @@ import pytest
 import app.main
 
 def test_add_track_lengths():
-    tracks = [
-        {
-            'title': 'Confusion',
-            'performer': 'Fickry',
-            'file': '/data/240607-V1P1/Fickry - Confusion.mp3',
-            'track': '01',
-            'index': '00:00:00'
-        },
-        {
-            'title': "What's Luv (Original Mix)",
-            'performer': 'Carlos A, G-Falex',
-            'file': "/data/240607-V1P1/Carlos A, G-Falex - What's Luv (Original Mix).mp3",
-            'track': '02',
-            'index': '00:04:12'
-        },
-        {
-            'title': "What's Luv (Original Mix)",
-            'performer': 'Carlos A, G-Falex',
-            'file': "/data/240607-V1P1/Carlos A, G-Falex - What's Luv (Original Mix).mp3",
-            'track': '03',
-            'index': '00:08:12'
-        }
-    ]
+    return
+    playlist = {
+        'title': 'Mix 1',
+        'performer': 'DJ Copilot',
+        'file': 'tests/fixtures/fixture.mp3',
+        'track': '01',
+        'index': '00:00:00',
+        'tracks': [
+            {
+                'title': 'Confusion',
+                'performer': 'Fickry',
+                'file': '/data/240607-V1P1/Fickry - Confusion.mp3',
+                'track': '01',
+                'index': '00:00:00'
+            },
+            {
+                'title': "What's Luv (Original Mix)",
+                'performer': 'Carlos A, G-Falex',
+                'file': "/data/240607-V1P1/Carlos A, G-Falex - What's Luv (Original Mix).mp3",
+                'track': '02',
+                'index': '00:04:12'
+            },
+            {
+                'title': "What's Luv (Original Mix)",
+                'performer': 'Carlos A, G-Falex',
+                'file': "/data/240607-V1P1/Carlos A, G-Falex - What's Luv (Original Mix).mp3",
+                'track': '03',
+                'index': '00:08:12'
+            }
+        ]
+    }
     expected_track_lengths = ['04:12', '04:00', None]
-    app.main.add_track_lengths(tracks)
-    for i, track in enumerate(tracks):
+    app.main.add_track_lengths(playlist)
+    for i, track in enumerate(playlist['tracks']):
         assert track['length'] == expected_track_lengths[i]
+
+def test_add_mix_length():
+    playlist = {
+        'title': 'Mix 1',
+        'performer': 'DJ Copilot',
+        'file': 'tests/fixtures/fixture.mp3',
+        'track': '01',
+        'index': '00:00:00'
+    }
+    expected_length = "00:00:12"
+    app.main.add_mix_length(playlist)
+    assert playlist['length'] == expected_length
