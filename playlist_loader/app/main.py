7c396dbef0f9de30af6ffce9de23f09962fb8716
Juni Samos <juni@Junis-MacBook-Pro.local>
2024-08-09 07:26:00 +0200

    Load Cue

--- playlist_loader/app/main.py
+++ playlist_loader/app/main.py
@@ -1,2 +1,17 @@
-def hello_world():
-    return "hello world"
+import argparse
+import cue
+
+def load_tracks(cue_path):
+    with open(cue_path) as f:
+        cue_string = f.read()
+    tracks = cue.parse_cue(cue_string)
+    return tracks
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cue_path", help="Path to the cue file")
+    args = parser.parse_args()
+
+    cue_path = args.cue_path
+    tracks = load_tracks(cue_path)
+    print(tracks)
